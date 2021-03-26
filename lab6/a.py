7523c5131695510a29fab7cf3d5bf27d7dc810f4
Rupesh Kalantre <rupu.kalantre@gmail.com>
2021-03-26 23:32:42 +0530

    added files

--- lab6/a.py
+++ lab6/a.py
@@ -4,7 +4,11 @@ from sklearn.svm import SVC
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 import pandas as pd
+import sys
 
+kernel_name = sys.argv[1]
+
+print("kernel Name : " , kernel_name)
 
 df = pd.read_csv("spambase.data")
 x = df.iloc[:,:-1]
@@ -18,15 +22,27 @@ x = pd.DataFrame(x)
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=42)
 
-svm = SVC(kernel="rbf", C = 2)
+svm = SVC(kernel=kernel_name, C = 4)
 
 svm.fit(x_train, y_train)
 
-y_predict = svm.predict(x_test)
+y_train_predict = svm.predict(x_train)
+y_test_predict = svm.predict(x_test)
 
-acc = accuracy_score(y_test, y_predict)
+print("train, test accuracies : ")
+print(accuracy_score(y_train, y_train_predict), accuracy_score(y_test, y_test_predict))
 
-print(acc)
+# printing in file
+# for i in range(1, 50) : 
+#     if kernel_name == "poly" : 
+#         svm = SVC(kernel=kernel_name, C = i, degree = 2)
+#     else : 
+#         svm = SVC(kernel=kernel_name, C = i)
+#     svm.fit(x_train, y_train)
+#     y_train_predict = svm.predict(x_train)
+#     y_test_predict = svm.predict(x_test)
+#     print(i, accuracy_score(y_train, y_train_predict), accuracy_score(y_test, y_test_predict))
+
 
 # svm.score(x_test, y_test)
 
